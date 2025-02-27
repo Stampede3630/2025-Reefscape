82acae1656d6e521ab5bc50436ffdf6367d0ee99
Caleb <49379586+ToothbrushB@users.noreply.github.com>
2025-02-27 10:11:52 -0600

    Advantage Kit (#1)
    * Akit-ified the swerve
    
    * Akit-ify the elevator
    
    * Akit-ify the manipulator and orchestra
    
    * startend
    
    * fix elevator
    
    * dashboard tune
    
    * advantage kit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,19 +10,36 @@ package frc.robot;
 import static edu.wpi.first.units.Units.Hertz;
 
 import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.AudioConfigs;
 import edu.wpi.first.units.measure.Frequency;
+import edu.wpi.first.wpilibj.RobotBase;
 
 public class Constants {
+  public static final Mode simMode = Mode.SIM;
+  public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
   public static final CANBus kSwerveCanBus = new CANBus("Swerve");
   public static final CANBus kManipulatorCanBus = new CANBus("rio");
-
   public static final int kFunnelMotorId = 0;
   public static final int kClimberMotorId = 0;
-
   public static final int kElevatorLeaderId = 17;
   public static final int kElevatorFollowerId = 18;
   public static final int kManipulatorMotorId = 1;
   public static final int kCanRangeId = 19;
+  public static final Frequency kUnimportantUpdateRate = Hertz.of(50);
 
-  public static final Frequency kNonSwerveUpdateRate = Hertz.of(250);
+  public static final AudioConfigs kAudioConfigs =
+      new AudioConfigs().withBeepOnConfig(true).withBeepOnBoot(true).withAllowMusicDurDisable(true);
+  public static double kSomewhatImportantUpdateRate = 100;
+  public static double kImportantUpdateRate = 250;
+
+  public enum Mode {
+    /** Running on a real robot. */
+    REAL,
+
+    /** Running a physics simulator. */
+    SIM,
+
+    /** Replaying from a log file. */
+    REPLAY
+  }
 }
---
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -0,0 +1,71 @@
+// Copyright (c) 2025 FRC 3630
+// https://github.com/Stampede3630
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.subsystems.elevator;
+
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.EqualsUtil;
+import java.util.function.DoubleSupplier;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.LoggedNetworkBoolean;
+
+public class Elevator extends SubsystemBase {
+  private static final String KEY = "Elevator";
+  private final ElevatorIO io;
+  private final ElevatorIOInputsAutoLogged inputs = new ElevatorIOInputsAutoLogged();
+  private final Alert leaderDisconnectedAlert =
+      new Alert("Elevator leader motor disconnected!", Alert.AlertType.kWarning);
+  private final Alert followerDisconnectedAlert =
+      new Alert("Elevator follower motor disconnected!", Alert.AlertType.kWarning);
+
+  private final LoggedNetworkBoolean coastOverride =
+      new LoggedNetworkBoolean(KEY + "/CoastOverride");
+  @AutoLogOutput private boolean coastModeEnabled = true;
+
+  public Elevator(ElevatorIO io) {
+    this.io = io;
+  }
+
+  public Command setPosition(DoubleSupplier position) {
+    return runOnce(() -> io.runPosition(position.getAsDouble()));
+  }
+
+  public boolean atGoal() {
+    return EqualsUtil.epsilonEquals(inputs.position, inputs.reference, 0.5);
+  }
+
+  private void setCoastMode(boolean enabled) {
+    if (coastModeEnabled == enabled) return;
+    coastModeEnabled = enabled;
+    io.setCoastMode(coastModeEnabled);
+  }
+
+  public Command upCommand() {
+    return setPosition(() -> inputs.appliedPosition + 2);
+  }
+
+  public Command downCommand() {
+    return setPosition(() -> inputs.appliedPosition - 2);
+  }
+
+  public Command seedPosition(DoubleSupplier position) {
+    return runOnce(() -> io.seedPosition(position.getAsDouble()));
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs(KEY, inputs);
+    leaderDisconnectedAlert.set(!inputs.leaderConnected);
+    followerDisconnectedAlert.set(!inputs.followerConnected);
+
+    setCoastMode(coastOverride.get());
+  }
+}
